3e9e2e9ccf911491742ec1e5f4f3a4c22135a153
Thiago Gomes <tsg.gomes9@gmail.com>
2022-06-02 13:49:19 -0400

    Versão 2.0 com atualização atomática dos status de 3 criptomoedas

--- home.js
+++ home.js
@@ -1,21 +1,27 @@
-const coin = document.querySelector('#coin')
+// const coin = document.querySelector('#coin')
 
-function showCoin(coin) {
-  document.querySelector('#high').value = coin.high
-  document.querySelector('#low').value = coin.low
-  document.querySelector('#buy').value = coin.buy
-  document.querySelector('#sell').value = coin.sell
-  document.querySelector('#open').value = coin.open
+var coins = ['btc', 'eth']
+
+function showCoin(coin, coinName) {
+  document.querySelector(`#${coinName}Buy`).innerHTML = coin.buy
+  document.querySelector(`#${coinName}Sell`).innerHTML = coin.sell
+  document.querySelector(`#${coinName}Open`).innerHTML = coin.open
+  document.querySelector(`#${coinName}High`).innerHTML = coin.high
+  document.querySelector(`#${coinName}Low`).innerHTML = coin.low
 }
 
-coin.addEventListener('click', () => {
-  setInterval(function () {
-    const search = coin.value
+setInterval(function () {
+  fetch(`https://www.mercadobitcoin.net/api/btc/ticker`)
+    .then(c => c.json())
+    .then(coinConsult => showCoin(coinConsult.ticker, 'btc'))
 
-    console.log(search)
+  fetch(`https://www.mercadobitcoin.net/api/eth/ticker`)
+    .then(c => c.json())
+    .then(coinConsult => showCoin(coinConsult.ticker, 'eth'))
 
-    fetch(`https://www.mercadobitcoin.net/api/${search}/ticker`)
-      .then(r => r.json())
-      .then(coin => showCoin(coin.ticker))
-  }, 1000)
-})
+  fetch(`https://www.mercadobitcoin.net/api/axs/ticker`)
+    .then(c => c.json())
+    .then(coinConsult => showCoin(coinConsult.ticker, 'axs'))
+}, 3000)
+
+// })
